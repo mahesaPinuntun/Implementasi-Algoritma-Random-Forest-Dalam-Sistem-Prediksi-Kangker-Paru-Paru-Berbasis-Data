49bc760f4a9bce72bc2f706cc4ff479629cd859f
EpsteinCPRepo <140386522+mahesaPinuntun@users.noreply.github.com>
2026-05-07 17:37:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -371,4 +371,4 @@ def debug_bundle():
 # ══════════════════════════════════════════════════════════════
 port = int(os.environ.get('PORT', 5000))
 print(f'Starting Flask on port {port}')
-app.run(host='0.0.0.0', port=port, debug=True)
+app.run(host='0.0.0.0', port=port, debug=False)
